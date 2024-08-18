b100ab2ee16e40b78a0027ce86efa71ea5973cdc
Karan <karanprince85@gmail.com>
2024-08-18 15:11:12 +0530

    save

--- JS Fundamentals/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ JS Fundamentals/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,4 +1,10 @@
-document.querySelector("button") 
-function handleClick() { 
-alert("I got clicked!");
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+for ( var i = 0 ; i < noOfDrumButtons ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() { 
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        } );
+
+
 }
+
