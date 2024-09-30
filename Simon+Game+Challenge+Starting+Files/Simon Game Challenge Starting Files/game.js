caae22b74ea0125f886e9d1787451b1a2a630396
Karan <karanprince85@gmail.com>
2024-09-30 10:20:40 +0530

    s

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -16,10 +16,41 @@ $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
 }
 
 
 );
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+
+            //5. Call nextSequence() after a 1000 millisecond delay.
+            setTimeout(function () {
+              nextSequence();
+            }, 1000);
+    
+          }
+    
+        } else {
+    
+          console.log("wrong");
+          playSound("wrong");
+          $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+    
+        }
+  
+
+}
+
 function nextSequence(){
 
     level++;
@@ -41,4 +72,10 @@ function playSound(){
         setTimeout(function () {
             $("#" + currentColor).removeClass("pressed");
           }, 100);
+    }
+
+    function startOver(){
+        level = 0;
+        gamePattern = [];
+        started = false;
     }
